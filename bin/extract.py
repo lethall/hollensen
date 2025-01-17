db6ea3615517d7950ab7c7d70ff7d8db861feefa
Lee Hall <javahall@gmail.com>
2025-01-17 09:38:41 -0500

    Transcribed with whisper-mps medium.en

--- bin/extract.py
+++ bin/extract.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
-
+import sys
 import json
+
+if len(sys.argv) != 2:
+    print("usage: extract <txt file name>", file=sys.stderr)
+    sys.exit(1)
 
 with open("output.json", "r") as f:
     o = json.load(f)
 
-with open("output.txt", "w") as f:
+with open(f"src/{sys.argv[1]}.txt", "w") as f:
     f.write(o["text"])
