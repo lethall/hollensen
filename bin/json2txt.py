2f6ab988b3b4f3f75a3eb5780dbad67b29a98eec
Lee Hall <lhall@javafoundry.com>
2025-02-22 17:37:45 -0500

    Better conditions

--- bin/json2txt.py
+++ bin/json2txt.py
@@ -7,9 +7,10 @@ with open("output.json", "r") as f:
 with open(sys.argv[1], "w") as fo:
     last_minute = None
     for s in segments:
-        minute = f"{s['end']//60:.0f}"
+        timestamp = s['end']
+        minute = timestamp//60
         text = s['text'].strip()
         print(text, file=fo)
-        if minute != last_minute and text.endswith(".") and minute != "0":
-            print(f"##### {f"{s['end']//60:02.0f}:{s['end']%60:02.0f}"}", file=fo)
+        if minute != last_minute and text[-1] in ".?!" and minute:
+            print(f"##### {timestamp//60:02.0f}:{timestamp%60:02.0f}", file=fo)
             last_minute = minute
