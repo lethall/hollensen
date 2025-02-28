0e58c267dd7d3ded01022d0c369cbe9c20b8ecf0
Lee Hall <javahall@gmail.com>
2025-02-28 13:21:33 -0500

    New web page

--- bin/gen_page.py
+++ bin/gen_page.py
@@ -25,6 +25,22 @@ if date:
     date = "/".join((date[4:6], date[6:], date[:4]))
     meta["date"] = date
 
+in_synopsis = False
+synopsis = []
+start_transcript = 0
+for ln in md_data:
+  start_transcript += 1
+  ln = ln.strip()
+  if in_synopsis:
+    if ln == "## Transcript":
+      break
+    synopsis.append(ln)
+    continue
+  if ln == "## Synopsis":
+    in_synopsis = True
+
+meta["synopsis"] = "</h3>\n<h3>".join(synopsis)
+
 TEMPLATE = '''
 <!DOCTYPE html>
 <html>
@@ -41,12 +57,14 @@ TEMPLATE = '''
   <center class="heading">
 
     <table>
-      <tr><td><img src=pulpit_sm.gif></td>
+      <tr><td><img src=left_image.gif></td>
         <td align="center"><h1>"${title}"</h1><h1>${from}</h1></td>
-        <td><img src=portrait_sm.gif></td></tr>
+        <td><img src=right_image.gif></td></tr>
       <tr><td></td><td align="center">This sermon was preached ${date} at Emanuel Lutheran Church in Marion, OH</td><td></td></tr>
       <tr><td></td><td align="center"><h2>${occasion}</h2></td><td></td></tr>
     </table>
+    <h3>${synopsis}</h3>
+    <br>
   </center>
   <audio id="audioPlayer" class="floating-audio" controls>
     <source src="/hollensen/${sermon_id}.mp3" type="audio/mpeg">
@@ -64,4 +82,4 @@ for k in meta.keys():
     page = page.replace("${" + k + "}", meta[k])
 
 with open(meta["sermon_id"] + ".html", "w") as f:
-    f.write(page.replace("${body}", markdown("".join(md_data))))
+    f.write(page.replace("${body}", markdown("".join(md_data[start_transcript:]))))
